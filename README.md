0a5eaf8dd62ee412af7c6c7792894963055a089d
Yangshen⚡Deng <yangshen.d@bupt.edu.cn>
2021-11-01 17:46:33 +0800

    switch to CMake and format add template

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Pratical examples of C++11
 ## Containers
 * [How to define key's class for std::map?](./src/learn_map.cpp)
 * [How to define key's class for std::unordered_map?](./src/learn_unordered_map.cpp)
-## design pattern
+## Design pattern
 * [How to implement thread-safe observer pattern?](./src/learn_observer.cpp)
+## Template
+* [How to count array's size?](./src/learn_raw_array.cpp)
--- src/learn_observer.cpp
+++ src/learn_observer.cpp
@@ -7,38 +7,38 @@
 #include <thread>
 
 class Observer;
+
 class Observerable;
 
-class Observer : public std::enable_shared_from_this<Observer>
-{
+class Observer : public std::enable_shared_from_this<Observer> {
 private:
     std::shared_ptr<Observerable> _obj;
 
 public:
     void update();
+
     void observe(const std::shared_ptr<Observerable> &obj);
 };
 
-class Observerable
-{
+class Observerable {
 private:
     std::vector<std::weak_ptr<Observer>> observers;
     std::mutex _mutex;
 
 public:
     void register_(std::weak_ptr<Observer> observer);
+
     void notifyObservers();
 };
 
 /* class Observer; */
 /*------------------------------------------------*/
-void Observer::update()
-{
+void Observer::update() {
     std::cout << "Observer::update" << std::endl;
 }
+
 /*------------------------------------------------*/
-void Observer::observe(const std::shared_ptr<Observerable> &obj)
-{
+void Observer::observe(const std::shared_ptr<Observerable> &obj) {
     obj->register_(shared_from_this());
 }
 /*------------------------------------------------*/
@@ -46,51 +46,42 @@ void Observer::observe(const std::shared_ptr<Observerable> &obj)
 
 /* class Observerable; */
 /*------------------------------------------------*/
-void Observerable::register_(std::weak_ptr<Observer> observer)
-{
+void Observerable::register_(std::weak_ptr<Observer> observer) {
     observers.push_back(observer);
 }
 
 /*------------------------------------------------*/
-void Observerable::notifyObservers()
-{
+void Observerable::notifyObservers() {
     std::lock_guard<std::mutex> lock(_mutex);
 
     auto it = observers.begin();
-    while (it != observers.end())
-    {
+    while (it != observers.end()) {
         auto sptr = it->lock();
-        if (sptr)
-        {
+        if (sptr) {
             sptr->update();
             ++it;
-        }
-        else
-        {
+        } else {
             it = observers.erase(it);
         }
     }
 }
+
 /*------------------------------------------------*/
 
 
-int main()
-{
+int main() {
     auto obj = std::make_shared<Observerable>();
     std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i)
-    {
+    for (int i = 0; i < 10; ++i) {
         threads.push_back(
-            std::thread([&]()
-                        {
-                            auto obs = std::make_shared<Observer>();
-                            obs->observe(obj);
-                            std::this_thread::sleep_for(std::chrono::seconds(2));
-                        }));
+                std::thread([&]() {
+                    auto obs = std::make_shared<Observer>();
+                    obs->observe(obj);
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                }));
     }
     obj->notifyObservers();
-    for (auto &thread : threads)
-    {
+    for (auto &thread: threads) {
         thread.join();
     }
 }
